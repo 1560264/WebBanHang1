bb68df3f8eaf3b92801551166ce14db033c088dc
DangKhoaDo <32297622+1560264@users.noreply.github.com>
2019-03-02 22:14:11 +0700

    Up

---
+++ WebSiteBanHang/WebSiteBanHang/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+namespace WebSiteBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult MenuPartial()
+        {
+            var lstSanPham = db.SanPham;
+            return PartialView(lstSanPham);
+        }
+	}
+}
---
+++ WebSiteBanHang/WebSiteBanHang/Controllers/SanPhamController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteBanHang.Models;
+namespace WebSiteBanHang.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        //
+        // GET: /SanPham/
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        public ActionResult SanPham1()
+        {
+            var lstSanPham = db.SanPham;
+            ViewBag.lstSanPham1 = lstSanPham;
+            return View();
+        }
+        public ActionResult SanPham2()
+        {
+            var lstSanPham = db.SanPham.Where(n => n.MaLoai == 2);
+            ViewBag.lstSanPham2 = lstSanPham;
+            return View();
+        }
+        public ActionResult SanPhamPartial()
+        {
+            //var lstSanPham = db.SanPham.Where(n => n.MaLoai == 1);
+            return PartialView();
+        }
+	}
+}
