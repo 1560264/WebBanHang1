176474a86a44412dcbd0c70fe36e2a82d9764901
DangKhoaDo <32297622+1560264@users.noreply.github.com>
2019-05-02 20:41:08 +0700

    up

--- WebSiteBanHang/WebSiteBanHang/Controllers/HomeController.cs
+++ WebSiteBanHang/WebSiteBanHang/Controllers/HomeController.cs
@@ -34,24 +34,58 @@ namespace WebSiteBanHang.Controllers
         [HttpPost]
         public ActionResult DangKy2(ThanhVien tv)
         {
+            db.ThanhVien.Add(tv);
+            db.SaveChanges();
+            ViewBag.CauHoi = new SelectList(LoadCauHoi());
             return View();
         }
 
         [HttpGet]
         public ActionResult DangKy2()
         {
+            ViewBag.CauHoi  = new SelectList(LoadCauHoi());
             return View();
         }
+        public List<string> LoadCauHoi()
+        {
+            List<string> lstCH = new List<string>();
+            lstCH.Add("Trường tiểu học đầu tiên?");
+            lstCH.Add("Món ăn ưa thích?");
+            lstCH.Add("Thức uống ưa thích?");
+            lstCH.Add("Ngày ưa thích trong năm?");
+            return lstCH;
+        }
         [HttpPost]
-        public ActionResult DangKy(ThanhVien tv)
+        public ActionResult DangKy3(ThanhVien tv)
         {
             return View();
         }
 
         [HttpGet]
-        public ActionResult DangKy()
+        public ActionResult DangKy3()
         {
             return View();
+        }
+        public ActionResult DangNhap(FormCollection f)
+        {
+            string sTaiKhoan = f["txtTenDangNhap"].ToString();
+            string sMatKhau = f["txtMatKhau"].ToString();
+            ThanhVien tv = new ThanhVien();
+            tv = db.ThanhVien.SingleOrDefault(n => n.TaiKhoan == sTaiKhoan && n.MatKhau == sMatKhau);
+            if (tv != null)
+            {
+                
+                Session["TaiKhoan"] = tv;
+                return RedirectToAction("Index");
+                //return Content("<script>window.location.reload();</script>");
+            }
+            return Content("Tên đăng nhập hoặc mật khẩu không hợp lệ");
+        }
+        
+        public ActionResult DangXuat()
+        {
+            Session["TaiKhoan"] = null;
+            return RedirectToAction("Index");
         }
 	}
 }
